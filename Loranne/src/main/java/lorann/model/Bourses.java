c8b34393cc0e8f4ca5c1087d2be2e71818c70f5a
flori <flori@10.59.73.113>
2016-06-13 12:11:48 +0200

    code

--- Loranne/src/main/java/lorann/model/Bourses.java
+++ Loranne/src/main/java/lorann/model/Bourses.java
@@ -1,5 +1,37 @@
 package lorann.model;
 
-public class Bourses {
+import java.awt.Image;
+import java.awt.Rectangle;
 
+import javax.swing.ImageIcon;
+
+public class Bourses {
+	int x,y;
+	Image Bourses;
+	
+	public Bourses(int Startx, int Starty){
+		x = Startx;
+		y = Starty;
+		
+		ImageIcon iBourses = new ImageIcon("Images/purse.jpg");
+		Bourses = iBourses.getImage();
+	}
+	
+
+	public Rectangle getBounds(){ //pour les collision
+		Rectangle Box = new Rectangle(x,y,34,34);
+		return Box;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public Image getImage(){
+		return Bourses;
+	}
 }
