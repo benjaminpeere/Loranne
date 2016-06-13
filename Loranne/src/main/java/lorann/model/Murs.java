2b94dc467444a8591127d257ab116492012aaf84
benjaminpeere <benjamin.peere@vacesi.fr>
2016-06-13 12:10:09 +0200

    classe mur

--- Loranne/src/main/java/lorann/model/Murs.java
+++ Loranne/src/main/java/lorann/model/Murs.java
@@ -1,5 +1,58 @@
 package lorann.model;
 
+import java.awt.Image;
+import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
+
 public class Murs {
 
+	int x,y;
+	Image Bone;
+	String BoneDir = "VERTICAL";
+	ImageIcon iMur = new ImageIcon("Images/bone.png");
+	ImageIcon iMurHorizon = new ImageIcon("Images/horizontal_bone.png");
+	ImageIcon iMurVerti = new ImageIcon("Images/vertical_bone.png");
+
+
+	public Murs(int Startx, int Starty){
+		x = Startx;
+		y = Starty;
+
+
+
+	}
+
+	public Rectangle getBounds(){
+		Rectangle Box = new Rectangle (x,y,34,34);
+		return Box;
+	}
+
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+
+	public String getDir(){
+		return BoneDir;
+	}
+
+	public Image getImage(){
+		if(BoneDir == "VERTICAL"){
+			Bone = iMur.getImage();
+		}
+		else if (BoneDir == "HORIZONTAL"){
+			Bone = iMurHorizon.getImage();
+		}
+		else if (BoneDir == "DROIT"){
+			Bone = iMur.getImage();
+		}
+		
+		return Bone;
+	}
+
 }
+
+
