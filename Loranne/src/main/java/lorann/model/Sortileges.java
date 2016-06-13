f4acf68d30e3136dfb5174392dc8af1c6ced25d5
flori <flori@10.59.73.113>
2016-06-13 15:22:03 +0200

    modification sortileges

--- Loranne/src/main/java/lorann/model/Sortileges.java
+++ Loranne/src/main/java/lorann/model/Sortileges.java
@@ -5,43 +5,32 @@ import java.awt.Rectangle;
 
 import javax.swing.ImageIcon;
 
-public class Sortileges  {
+public class Sortileges {
 	int x,y;
-	String SortilegeDir = "BAS";
-	Image Sortilege;
-	ImageIcon iFireball1 = new ImageIcon ("Image/fireball_1.png");
-	ImageIcon iFireball2 = new ImageIcon ("Image/fireball_2.png");
-	ImageIcon iFireball3 = new ImageIcon ("Image/fireball_3.png");
-	ImageIcon iFireball4 = new ImageIcon ("Image/fireball_4.png");
-	ImageIcon iFireball5 = new ImageIcon ("Image/fireball_5.png");
-
+	Image Sortileges;
+	
 	public Sortileges(int Startx, int Starty){
 		x = Startx;
-		y = Starty;	
+		y = Starty;
+		
+		ImageIcon iSortileges = new ImageIcon("Images/fire_1.png");
+		Sortileges = iSortileges.getImage();
 	}
 
-	public Rectangle getBounds(){
-		Rectangle Box = new Rectangle (x,y,32,32);
+	public Rectangle getBounds(){ //pour les collisions
+		Rectangle Box = new Rectangle(x,y,32,32);
 		return Box;
 	}
-
-	public int getX() {
+	
+	public int getX(){
 		return x;
 	}
-	public int getY() {
+	
+	public int getY(){
 		return y;
 	}
-	public String getDir() {
-		return SortilegeDir;
-	}
-
-	public void setX(int newX) {
-		this.x = newX;
-	}
-	public void setY(int newY) {
-		this.y = newY;
-	}
-	public void setDir(String newDir) {
-		this.SortilegeDir = newDir;
+	
+	public Image getImage(){
+		return Sortileges;
 	}
 }
