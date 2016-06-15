16e8a86dd7b5630b4e28d6e57ac3106a932508ff
benjaminpeere <benjamin.peere@vacesi.fr>
2016-06-15 17:43:33 +0200

    deplacement

--- Loranne/src/main/java/lorann/model/GameBoard.java
+++ Loranne/src/main/java/lorann/model/GameBoard.java
@@ -246,16 +246,20 @@ public class GameBoard extends JPanel implements KeyListener {
 
 	public void pathToLorann(){
 		if(demon1.getX()<lorann.getX()){
-			demon1.setX(4);
+			demon1.setX(1);
+			demon1.MoveDemon();
 		}
 		else if(demon1.getX() > lorann.getX()){
-			demon1.setX(-4);
+			demon1.setX(-1);
+			demon1.MoveDemon();
 		}
 		else if(demon1.getY() < lorann.getY()){
-			demon1.setY(4);
+			demon1.setY(1);
+			demon1.MoveDemon();
 		}
 		else if(demon1.getY() > lorann.getY()){
-			demon1.setY(-4);
+			demon1.setY(-1);
+			demon1.MoveDemon();
 		}
 		repaint();
 		
